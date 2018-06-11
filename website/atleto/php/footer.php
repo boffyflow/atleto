be90f54d739ca20f2a9c9c243c8dfd2cd3c5ed9f
boffyflow <boffyflow@users.noreply.github.com>
2018-06-11 15:40:13 -0700

    updated footer

--- website/atleto/php/footer.php
+++ website/atleto/php/footer.php
@@ -3,8 +3,8 @@
 	include $_SERVER[ 'DOCUMENT_ROOT'] . "/php/global_inc.php";
 	include $_SERVER[ 'DOCUMENT_ROOT'] . "/php/db/general.php";
 	
-    function footer()
-    {
+  function footer()
+  {
 		global $dbfilename;
 		
 		$db = new SQLite3( $dbfilename);
@@ -12,6 +12,6 @@
 		$r = cal_from_jd( $db->querySingle( "select max(day) from runs"), CAL_GREGORIAN);
 		
 		echo "Number of runs: ". numRuns() . ", Last run on: " . $r['dayname'] . ", " . $r['abbrevmonth'] . " " . $r['day'] . " " . $r['year'];
-		echo "<br>&copy; 2013-2016 Robert Uebbing";
-    }
+		echo "<br>&copy; 2013-2018 Robert Uebbing";
+  }
 ?>    
