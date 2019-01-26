09fec33611f19729097788ad39c3e306aff7f667
boffyflow <boffyflow@users.noreply.github.com>
2019-01-25 16:55:04 -0800

    refactored code and added jupyter notebook

--- python/atleto.py
+++ python/atleto.py
@@ -3,9 +3,6 @@ import jdutil as jd
 from datetime import time
 import sqlite3
 import math
-import plotly.plotly as py
-import plotly.graph_objs as go
-
 
 class aveHR:
     def __init__(self):
@@ -20,7 +17,6 @@ class aveHR:
         return self.hr / self.tdist
 
 def jdate(day):
-
     datet = jd.jd_to_datetime(day)
     return datet.strftime( '%Y-%m-%d')
 
@@ -35,29 +31,24 @@ def pace(t, dist):
     timet = time( 0, mins, round(secs))
     return timet.strftime( '%M:%S')
 
-def main():
+class atleto:
+    def __init__(self, fname):
+        self.filename = fname
+        
+    def runs(self):
+        conn = sqlite3.connect( self.filename)
 
-    conn = sqlite3.connect("atleto_data.atl")
-    conn.create_function('jdate', 1, jdate)
-    conn.create_function('pace', 2, pace)
-    conn.create_aggregate('avehr', 2, aveHR)
+        conn.create_function('jdate', 1, jdate)
+        conn.create_function('pace', 2, pace)
+        conn.create_aggregate('avehr', 2, aveHR)
 
-    sqlite3.enable_callback_tracebacks(True) 
+        sqlite3.enable_callback_tracebacks(True) 
 
-    runs = pd.read_sql_query("SELECT jdate(day) AS Date,SUM(dist) AS Distance,\
-                                SUM(t) AS Time,PACE(SUM(t),SUM(dist)) AS Pace,\
-                                AVEHR(hr,dist) AS Heartrate\
-                                FROM run_splits\
-                                INNER JOIN runs\
-                                ON runs.id=run_splits.run_id\
-                                GROUP BY run_splits.run_id\
-                                ORDER BY day ASC", conn)
-    runs.set_index('Date',inplace=True)
-
-    runsyear = runs['2016-01-01':'2017-11-30']
-    print(runsyear[(runsyear['Distance'] >= 5000.0) &\
-            (round(runsyear['Heartrate']) <= 140) &\
-            (round(runsyear['Heartrate']) >= 135)])
-
-if __name__ == '__main__':
-    main()    
+        return pd.read_sql_query("SELECT jdate(day) AS Date,SUM(dist) AS Distance,\
+                                        SUM(t) AS Time,PACE(SUM(t),SUM(dist)) AS Pace,\
+                                        AVEHR(hr,dist) AS Heartrate\
+                                        FROM run_splits\
+                                        INNER JOIN runs\
+                                        ON runs.id=run_splits.run_id\
+                                        GROUP BY run_splits.run_id\
+                                        ORDER BY day ASC", conn)
