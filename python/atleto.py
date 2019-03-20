4e7eb963508fe4b698716206186623738181e324
boffyflow <boffyflow@users.noreply.github.com>
2019-03-20 15:27:31 -0700

    Update atleto.py

--- python/atleto.py
+++ python/atleto.py
@@ -6,7 +6,6 @@ import sqlite3
 import math
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
-
 class aveHR:
     def __init__(self):
         self.t = 0
@@ -38,8 +37,7 @@ class atleto:
     def __init__(self, fname):
         self.filename = fname
     
-    def days( self, sdate, edate):
-
+    def physicals( self, sdate, edate):
         sjulian = jd.datetime_to_jd( sdate)
         ejulian = jd.datetime_to_jd( edate)
         
@@ -51,13 +49,54 @@ class atleto:
 
         for index, row in df.iterrows():
             df.at[index, 'Date'] = jdate( df.at[index,'day'])
+
         df = df.drop( columns=['day'])
-        df = df.set_index( 'Date')
+        df['Date'] = pd.to_datetime( df['Date'])
         df['Weight'] = df['Weight'] * 0.001
         df['Bodyfat'] = df['Bodyfat'] * 0.1
 
         print( 'from julian date - from:', sjulian, 'to:', ejulian)
         print( 'from calendar date - from:',jd.jd_to_date( sjulian, '%Y-%m-%d'), 'to:', jd.jd_to_date( ejulian, '%Y-%m-%d'))
+
+        return df
+
+    def runs( self, sdate, edate):
+        sjulian = jd.datetime_to_jd( sdate)
+        ejulian = jd.datetime_to_jd( edate)
+
+        conn = sqlite3.connect( self.filename)
+
+        conn.create_function('jdate', 1, jdate)
+        conn.create_function('pace', 2, pace)
+        conn.create_aggregate('avehr', 2, aveHR)
+
+        sqlite3.enable_callback_tracebacks(True) 
+
+        querystring = 'SELECT jdate(day) AS Date,SUM(dist) AS Distance,\
+                                        SUM(t) AS Time,PACE(SUM(t),SUM(dist)) AS Pace,\
+                                        AVEHR(hr,t) AS Heartrate\
+                                        FROM run_splits\
+                                        INNER JOIN runs\
+                                        ON runs.id=run_splits.run_id\
+                                        WHERE day<=%s AND day>=%s\
+                                        GROUP BY run_splits.run_id\
+                                        ORDER BY day ASC' % (ejulian,sjulian)
+
+        df = pd.read_sql_query( querystring, conn)
+        
+        df['Date'] = pd.to_datetime( df['Date'])
+        df['Pace'] = pd.to_datetime( df['Pace'], format='%M:%S')
+        df = df.round({'Heartrate':0})
+
+        return df
+
+    def days( self, sdate, edate):
+        
+        phys = self.physicals( sdate, edate)
+        runs = self.runs( sdate, edate)
+
+        df = pd.merge_ordered( phys, runs, on='Date', how='left')
+        df = df.set_index( 'Date')
 
         return df
 
@@ -70,31 +109,8 @@ class atleto:
     def years():
         return
 
-    def runs(self):
-        conn = sqlite3.connect( self.filename)
 
-        conn.create_function('jdate', 1, jdate)
-        conn.create_function('pace', 2, pace)
-        conn.create_aggregate('avehr', 2, aveHR)
-
-        sqlite3.enable_callback_tracebacks(True) 
-
-        df = pd.read_sql_query("SELECT jdate(day) AS Date,SUM(dist) AS Distance,\
-                                        SUM(t) AS Time,PACE(SUM(t),SUM(dist)) AS Pace,\
-                                        AVEHR(hr,t) AS Heartrate\
-                                        FROM run_splits\
-                                        INNER JOIN runs\
-                                        ON runs.id=run_splits.run_id\
-                                        GROUP BY run_splits.run_id\
-                                        ORDER BY day ASC", conn)
-        
-        df['Date'] = pd.to_datetime( df['Date'])
-        df['Pace'] = pd.to_datetime( df['Pace'], format='%M:%S')
-        df = df.round({'Heartrate':0})
-        
-        return df
-    
-def plotEasyPace(df):
+def plotEasyPace( df):
    
     # Initialize Figure and Axes object
     # Get current size
