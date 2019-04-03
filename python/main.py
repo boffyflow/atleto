00ec94d3880a4eb7e37a8322bafaec073b6d11a1
boffyflow <boffyflow@users.noreply.github.com>
2019-04-03 15:58:02 -0700

    Update main.py

--- python/main.py
+++ python/main.py
@@ -11,11 +11,16 @@ ed = dt.datetime.strptime( config['Default']['enddate'], '%Y-%m-%d')
 
 a = atl.atleto( config['Default']['file'], sd, ed)
 
-df = a.aggregate( config['Default']['aggregate'])
+#df = a.aggregate( config['Default']['aggregate'])
 #print( df)
 
-atl.plotDistPace( df)
+#atl.plotDistPace( df)
 
-runs = a.runs()
+#runs = a.runs()
 
-atl.plotEasyPace( runs)
+#atl.plotEasyPace( runs)
+
+print( 'VDOT:', round( atl.vdot( 10000, 2323), 1))
+print( 'VDOT time:', round( atl.vdotTime( 50, 10000)))
+print( 'LT pace:', round( atl.LTpace( 47.6)))
+print( 'RRSS:', round( atl.rrss( 4016, 89)))
