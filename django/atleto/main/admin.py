c091db64c05178b3a9cb3aecfe64383707b35520
boffyflow <boffyflow@gmail.com>
2019-04-07 19:34:07 -0700

    safety check in

--- django/atleto/main/admin.py
+++ django/atleto/main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Athlete
+from .models import Athlete, Run, Location, RunSplit
 
 admin.site.register( Athlete)
+admin.site.register( Run)
+admin.site.register( Location)
+admin.site.register( RunSplit)
--- django/atleto/main/models.py
+++ django/atleto/main/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models import Sum
+
+class RunSplit( models.Model):
+    dist = models.FloatField()
+    t = models.FloatField()
+    heartrate = models.IntegerField( default=0)
+    notes = models.TextField( blank=True, null=True)
+
+    def __str__( self):
+        return 'runsplit'
 
 class Athlete( models.Model):
     firstname = models.CharField( max_length=200)
     lastname = models.CharField( max_length=200)
     dob = models.DateField()
     height = models.IntegerField( default=175)
+    runs = models.ForeignKey( 'Run', on_delete=models.SET_NULL, null=True)
 
     def __str__( self):
         return self.firstname + ' ' + self.lastname
@@ -12,10 +24,17 @@ class Athlete( models.Model):
 class Run( models.Model):
     starttime = models.DateTimeField()
     endtime = models.DateTimeField()
-    location = models.ForeignKey( 'Location', on_delete=models.SET_NULL, null=True)
+    location = models.OneToOneField( 'Location', on_delete=models.SET_NULL, null=True)
+    splits = models.ForeignKey( 'RunSplit', on_delete=models.SET_NULL, null=True)
+
+    def __str__( self):
+        return self.location.__str__()
+
+    def distance( self, unit='km'):
+        return RunSplit.objects.aggregate( Sum( 'dist'))
 
 class Location( models.Model):
     name = models.CharField( max_length=200)
 
     def __str__( self):
-        return name
+        return self.name
